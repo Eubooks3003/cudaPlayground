58344b5cf12fc46c8441ba1131d9c2da8290acd7
Eubooks3003 <89107670+Eubooks3003@users.noreply.github.com>
2022-07-19 10:40:14 -0400

    Create nll_loss_forward.cu

---
+++ nll_loss_forward.cu
@@ -0,0 +1,60 @@
+constexpr int NLL_LOSS_THREADS = 32;
+
+#define AT_DISPATCH_NLL_LOSS_INDEX_TYPES(TYPE, NAME, ...)                   \
+  [&] {                                                                     \
+    at::ScalarType _it = TYPE;                                              \
+    RECORD_KERNEL_FUNCTION_DTYPE(NAME, _it)                                 \
+    switch (_it) {                                                          \
+      AT_PRIVATE_CASE_TYPE_USING_HINT(NAME, at::ScalarType::Byte, uint8_t, index_t, __VA_ARGS__) \
+      AT_PRIVATE_CASE_TYPE_USING_HINT(NAME, at::ScalarType::Long, int64_t, index_t, __VA_ARGS__)\
+      default:                                                              \
+        AT_ERROR(#NAME, " not implemented for '", toString(_it), "'");      \
+    }                                                                       \
+  }()
+
+template <typename scalar_t, typename accscalar_t, typename index_t>
+__global__ void nll_loss_forward_reduce_cuda_kernel_2d(
+    scalar_t* output,
+    scalar_t* total_weight,
+    scalar_t* input,
+    index_t* target,
+    scalar_t* weights,
+    bool size_average,
+    int nframe,
+    int ndim,
+    int n_classes,
+    int64_t ignore_index) {
+  // NOLINTNEXTLINE(cppcoreguidelines-init-variables)
+  __shared__ accscalar_t sh_inputs[NLL_LOSS_THREADS],
+      acc_weight[NLL_LOSS_THREADS];
+
+  sh_inputs[threadIdx.x] = static_cast<accscalar_t>(0);
+  acc_weight[threadIdx.x] = static_cast<accscalar_t>(0);
+  for (int i = threadIdx.x; i < nframe; i += NLL_LOSS_THREADS) {
+    int t = target[i];
+    if (t != static_cast<int>(ignore_index)) {
+      CUDA_KERNEL_ASSERT(t >= 0 && t < n_classes);
+      scalar_t cur_weight =
+          weights != nullptr ? weights[t] : static_cast<scalar_t>(1);
+      sh_inputs[threadIdx.x] -= input[i * ndim + t] * cur_weight;
+      acc_weight[threadIdx.x] += cur_weight;
+    }
+  }
+
+  __syncthreads();
+
+  if (threadIdx.x == 0) {
+    accscalar_t output_acc = 0;
+    accscalar_t total_weight_acc = 0;
+    for (int i = 0; i < NLL_LOSS_THREADS; ++i) {
+      output_acc += sh_inputs[i];
+      total_weight_acc += acc_weight[i];
+    }
+    *total_weight = static_cast<scalar_t>(total_weight_acc);
+    if (size_average) {
+      *output = static_cast<scalar_t>(output_acc / total_weight_acc);
+    } else {
+      *output = static_cast<scalar_t>(output_acc);
+    }
+  }
+}
