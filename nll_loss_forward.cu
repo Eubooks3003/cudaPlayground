3523eeaf8325f94ef4b85c13425535b155a60e82
Eubooks3003 <89107670+Eubooks3003@users.noreply.github.com>
2022-07-26 13:19:00 -0400

    Update nll_loss_forward.cu

--- nll_loss_forward.cu
+++ nll_loss_forward.cu
@@ -12,30 +12,30 @@ constexpr int NLL_LOSS_THREADS = 32;
     }                                                                       \
   }()
 
-template <typename scalar_t, typename accscalar_t, typename index_t>
+//template <typename scalar_t, typename accscalar_t, typename index_t>
 __global__ void nll_loss_forward_reduce_cuda_kernel_2d(
-    scalar_t* output,
-    scalar_t* total_weight,
-    scalar_t* input,
-    index_t* target,
-    scalar_t* weights,
+    float* output,
+    float* total_weight,
+    float* input,
+    long* target,
+    float* weights,
     bool size_average,
     int nframe,
     int ndim,
     int n_classes,
     int64_t ignore_index) {
   // NOLINTNEXTLINE(cppcoreguidelines-init-variables)
-  __shared__ accscalar_t sh_inputs[NLL_LOSS_THREADS],
+  __shared__ float sh_inputs[NLL_LOSS_THREADS],
       acc_weight[NLL_LOSS_THREADS];
 
-  sh_inputs[threadIdx.x] = static_cast<accscalar_t>(0);
-  acc_weight[threadIdx.x] = static_cast<accscalar_t>(0);
+  sh_inputs[threadIdx.x] = static_cast<float>(0);
+  acc_weight[threadIdx.x] = static_cast<float>(0);
   for (int i = threadIdx.x; i < nframe; i += NLL_LOSS_THREADS) {
     int t = target[i];
     if (t != static_cast<int>(ignore_index)) {
       CUDA_KERNEL_ASSERT(t >= 0 && t < n_classes);
       scalar_t cur_weight =
-          weights != nullptr ? weights[t] : static_cast<scalar_t>(1);
+          weights != nullptr ? weights[t] : static_cast<float>(1);
       sh_inputs[threadIdx.x] -= input[i * ndim + t] * cur_weight;
       acc_weight[threadIdx.x] += cur_weight;
     }
@@ -50,11 +50,11 @@ __global__ void nll_loss_forward_reduce_cuda_kernel_2d(
       output_acc += sh_inputs[i];
       total_weight_acc += acc_weight[i];
     }
-    *total_weight = static_cast<scalar_t>(total_weight_acc);
+    *total_weight = static_cast<float>(total_weight_acc);
     if (size_average) {
-      *output = static_cast<scalar_t>(output_acc / total_weight_acc);
+      *output = static_cast<float>(output_acc / total_weight_acc);
     } else {
-      *output = static_cast<scalar_t>(output_acc);
+      *output = static_cast<float>(output_acc);
     }
   }
 }
